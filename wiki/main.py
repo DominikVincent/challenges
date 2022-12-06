e734322fc3e93df5b11ab25b9e15176440b575cd
dh <asdf>
2022-12-06 19:23:23 +0100

    Add webgui tool

--- wiki/main.py
+++ wiki/main.py
@@ -8,6 +8,9 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import re
+from pywebio.input import input, FLOAT, textarea, radio, actions
+from pywebio.output import put_text, put_markdown, put_image, use_scope, put_button
+from pywebio.session import set_env
 
 """
 Queries webpage and returns it
@@ -28,12 +31,11 @@ def query_webpage(webpage: str) -> str:
 
 
 def random_webpage():
-    # soup = query_webpage('https://en.wikipedia.org/wiki/Special:Random')
-    soup = query_webpage('https://en.wikipedia.org/wiki/Jean_Paul')
+    soup = query_webpage('https://en.wikipedia.org/wiki/Special:Random')
+    # soup = query_webpage('https://en.wikipedia.org/wiki/Asakent')
+    # soup = query_webpage('https://en.wikipedia.org/wiki/Criollas_de_Caguas')
+    # soup = query_webpage('https://en.wikipedia.org/wiki/Jean_Paul')
     return soup
-
-def has_image() -> bool:
-    pass
 
 def get_image(image_url: str):
     # Open the url image, set stream to True, this will return the stream content.
@@ -44,11 +46,12 @@ def get_image(image_url: str):
 
     # Check if the image was retrieved successfully
     if r.status_code == 200:
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
         bytes_im = io.BytesIO(r.content)
         img_np = np.array(Image.open(bytes_im))
+        # TODO fix svg.png images only having one channel when opening with PIL and not with cv2.
         print(img_np.shape)
-        if img_np.shape[-1] != 3:
+        if img_np.shape[-1] != 3 and img_np.shape[-1] != 4 and img_np.shape[-1] != 1 and len(img_np.shape) != 2:
+            print(f"Img is not 1, 3 or 4 channel returning None for {image_url}")
             return
         return img_np
 
@@ -60,12 +63,19 @@ def get_image(image_url: str):
 Expects lists of image urls. Then plots all images with their corresponding url text
 """
 def show_images(images):
+    if len(images) == 0:
+        return
     fig, axs = plt.subplots(len(images))
-    for image_url, ax in zip(images, axs):
+    if not isinstance(axs, np.ndarray):
+        axs = [axs]
+
+    for image, ax in zip(images, axs):
+        image_url = image["src"]
+        ax.set_title(f"{image_url}\nalt: {image['alt']}")
         img = get_image(image_url)
         if img is not None:
+            print(f"Image shape: {img.shape}")
             ax.imshow(img)
-            ax.set_title(image_url)
     plt.show()
 
 
@@ -73,39 +83,67 @@ def extract_images(soup):
     # Extract the images
     # divs = [div for div in soup.find_all('div', {'class': 'thumb tright'})],
     # images = [img['src'] for img in divs.find_all('img')]
-    images = [img['src'] for img in soup.find_all('img')]
+    images = [{"src": img['src'], "alt": img['alt']} for img in soup.find_all('img')]
 
-    #['/250px-']
+    # TODO consider removing duplicates. But what is a duplicate alt texts are context dependent so the same src file
+    #  might have different alt texts in different contexts
+
     image_urls = []
     for img in images:
-        matches = re.findall(r'/\d+px-', img)
-        if len(matches) == 1:
-            match = matches[0]
-            image_size = int(match[1:-3])
-            if image_size > 100:
+        test = re.findall(r'/(\d+)px-', img["src"])
+        if len(test) == 1:
+            image_size = int(test[0])
+            if image_size > 0:
                 image_urls.append(img)
+            else:
+                print(f"Sorting out image because it is too small {img}")
+        else:
+            print(f"Image does not contain pixel size. Sorting out: {img}")
 
-    title = soup.find('title')
+    title = soup.find('title').contents[0]
 
     return image_urls, title
 
-def extract_all_images(page_soup):
-    img_tags = page_soup.find_all('img')
-    urls = [img['src'] for img in img_tags]
 
-    for url in urls:
-        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-        if not filename:
-            print("Regex didn't match with the url: {}".format(url))
-            continue
-        with open(filename.group(1), 'wb') as f:
-            if 'http' not in url:
-                # sometimes an image source can be relative
-                # if it is provide the base url which also happens
-                # to be the site variable atm.
-                url = '{}{}'.format(site, url)
-            response = requests.get(url)
-            f.write(response.content)
+
+def user_updates():
+    set_env(title="Alt Text Tool")
+
+    finished = False
+    while not finished:
+        soup = random_webpage()
+
+        images, title = extract_images(soup)
+        print("title: ", title)
+        with use_scope('wikipage', clear=True):
+            put_markdown(f'# Alt Text Update Tool: {title}')
+
+            for image in images:
+                with use_scope("image", clear=True):
+                    put_markdown('## Please add alt text')
+
+                    put_image(image["src"])
+
+                    if image["alt"] == "":
+                        put_text("The image has no alt text please add it.")
+                        text = input('Alt Text', rows=3, placeholder='Add the alt text here')
+                        image["alt"] = text
+                    else:
+                        put_text(f"Current alt text is: '{image['alt']}'. Do you think that fits?")
+                        answer = actions('Does the alt text fit?', [{"label":'Yes', "value": True, "color":"primary"},
+                                                                    {"label":'No', "value": False, "color":"warning"}])
+                        if not answer:
+                            text = input('Suggest New Alt Text', rows=3, placeholder='Add the alt text here')
+                            image["alt"] = text
+
+            print(images)
+            finished = actions("Current wiki page done. Do you want to fix another.", [{"label":'Yes', "value": False, "color":"primary"},
+                                                                    {"label":'No', "value": True, "color":"warning"}])
+            print("finished: ", finished)
+
+    with use_scope('wikipage', clear=True):
+        put_markdown(f'# Thank you! See you next time')
+
 
 def main():
 
@@ -120,6 +158,7 @@ def main():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    # main()
+    user_updates()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
